090990be97b13979b8ea8d6ee68624616935710e
loic <freer-cleat-0i@icloud.com>
2024-01-21 00:19:44 +0100

    Update note range to match original game

--- Source/Scripts/trombone.lua
+++ Source/Scripts/trombone.lua
@@ -5,15 +5,16 @@ function getPlayerPosition()
     return 100 - math.abs((playdate.getCrankPosition() - 180) / 1.8)
 end
 
-local pitchHighBound = 698.46 -- F5
-local pitchLowBound = 82.41 -- E2
+-- https://trombone.wiki/#/creating-charts
+-- "Midi notes should be in the range 47 to 73 to match the game."
+local pitchHighBound = 587.33 -- MIDI note 74
+local pitchLowBound = 116.54 -- MIDI note 46
 
 function getPitch(position)
     return 	pitchHighBound - (pitchHighBound - pitchLowBound) * position / 100
 end
 
 function startTooting(pitch)
-    tromboneSynth:noteOff()
     tromboneSynth:playNote(pitch)
 end
 
