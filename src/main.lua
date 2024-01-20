9b6bdbfa6dab773f5e726c2147adf58a4d86e0e8
Mickael Mogmi Bidon <mogmi95@gmail.com>
2024-01-20 15:58:19 +0100

    Added basic display and player controls

--- src/main.lua
+++ src/main.lua
@@ -1,3 +1,51 @@
+import "CoreLibs/graphics"
+
+local gfx <const> = playdate.graphics
+
+-- Vertical position of the dot, from 0 (top) to 100 (bottom)
+local playerPosition = 0
+
+function updatePlayerPosition()
+    local change = playdate.getCrankChange()
+    if change ~= 0 then
+        playerPosition = math.min(100, math.max(0, playerPosition + change))
+    end
+end
+
+function initGame()
+end
+
+function updateDisplay()
+    -- Clearing the screen
+    gfx.setColor(gfx.kColorWhite)
+    gfx.fillRect(0, 0, 400, 240)
+
+    -- Drawing the vertical left bar
+    gfx.setColor(gfx.kColorBlack)
+    gfx.drawLine(20, 20, 20, 220)
+    gfx.drawLine(16, 20, 24, 20)
+    gfx.drawLine(16, 220, 24, 220)
+
+    -- Drawing the player (on the left bar)
+    local playerCircleX = 20
+    local playerCircleY = 20 + 2 * playerPosition
+    local playerCircleRadius = 5
+
+    local drawingPlayerFunction = gfx.drawCircleAtPoint
+    if playdate.buttonIsPressed("up") or
+        playdate.buttonIsPressed("down") or
+        playdate.buttonIsPressed("left") or
+        playdate.buttonIsPressed("right")
+    then
+        drawingPlayerFunction = gfx.fillCircleAtPoint
+    end
+
+    drawingPlayerFunction(playerCircleX, playerCircleY, playerCircleRadius)
+end
+
+-- Gameplay loop
+initGame()
 function playdate.update()
-    print("Hello world!")
+    updatePlayerPosition()
+    updateDisplay()
 end
